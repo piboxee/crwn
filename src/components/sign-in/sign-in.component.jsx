ef888c9e03e706b0983b20659b62247501b78f1c
Valentin Piho <pibox.ee@gmail.com>
2020-02-15 12:06:08 +0200

    Sign up with email and password added

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
 
-import {singInWithGoogle} from '../../firebase/firebase.utils'
+import {auth, singInWithGoogle} from '../../firebase/firebase.utils'
 
 import './sign-in.styles.scss'
 
@@ -17,10 +17,19 @@ class SignIn extends Component {
         }
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault()
 
-        this.setState({email: '', password: ''})
+        const {email, password} = this.state
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({email: '', password: ''})
+        } catch (error) {
+            console.log(error)
+        }
+
+        
     }
 
     handleChange = (event) => {
