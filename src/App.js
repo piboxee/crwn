967612586692a356d7a8f752a9a1f747637f0a94
Valentin Piho <pibox.ee@gmail.com>
2020-02-18 22:24:47 +0200

    Checkout page and checkout items added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import {createStructuredSelector} from 'reselect'
 
 import './App.css';
 
-import Header from './components/header/header.component'
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import CheckoutPage from './pages/checkout/checkout.component';
+
+import Header from './components/header/header.component'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 import {setCurrentUser} from './redux/user/user.actions'
 import {selectCurrentUser} from './redux/user/user.selector'
+
 
 class App extends Component {
 
@@ -49,8 +52,9 @@ class App extends Component {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
         <Route exact path='/signin' render= {() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
+        <Route exact path='/checkout' component={CheckoutPage} />
       </Switch>
     </div>
   )}
