8a99ad6782050d344346c5a2850502767e62bbf6
Valentin Piho <pibox.ee@gmail.com>
2020-02-23 11:49:32 +0200

    Shop component state moved to Redux state

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,23 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {connect} from 'react-redux'
+import {createStructuredSelector} from 'reselect'
+
+import {selectCollections} from '../../redux/shop/shop.selectors'
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component'
 
-import SHOP_DATA from './shop.data'
-
-class ShopPage extends Component {
-    constructor() {
-        super()
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const {collections} = this.state
-        return <div className='shop-page'>
+const ShopPage = ({collections}) => (
+        <div className='shop-page'>
             {
                 collections.map(({id, ...otherCollectionProps}) => (
                     <CollectionPreview key={id} {...otherCollectionProps} />
                 ))
             }
         </div>
-    }
-}
+)
 
-export default ShopPage
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
+
+export default connect(mapStateToProps)(ShopPage)
