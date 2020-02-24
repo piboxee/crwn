fda673b00a1c2a78cb33d2e8d97a368b11b49eb7
Valentin Piho <pibox.ee@gmail.com>
2020-02-24 12:05:59 +0200

    Stripe payment added for front-end. Favicon added.

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 1000
+    const publishableKey ='pk_test_vvjkFbvfSSU1PwcV1ocfFPlm00etYpTD9L'
+
+    const onToken = token => {
+        console.log(token)
+        alert('Payment Successful')
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='http://sendeyo.com/up/d/f3eb2117da'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton
