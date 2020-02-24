cb8a2730c707daf0e93ff34ff29e7f00aa4fdc28
Valentin Piho <pibox.ee@gmail.com>
2020-02-24 14:22:46 +0200

    Refactored from SASS to Styled-components Header, Custom Button, Homepage

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
 import {createStructuredSelector} from 'reselect'
 
 import {auth} from '../../firebase/firebase.utils'
@@ -11,30 +10,30 @@ import {selectCurrentUser} from '../../redux/user/user.selector'
 
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 
-import './header.styles.scss'
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink} from './header.styles'
 
 const Header = ({currentUser, hidden}) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
+    <HeaderContainer>
+        <LogoContainer to='/'>
             <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/shop'>
+            </OptionLink>
+            <OptionLink to='/shop'>
                 CONTACT
-            </Link>
+            </OptionLink>
                 {
                     currentUser ? (
-                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                    <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
                     ) : (
-                    <Link className='option' to='/signin'>SIGN IN</Link>
+                    <OptionLink to='/signin'>SIGN IN</OptionLink>
                 )}
                 <CartIcon />
-        </div>
+        </OptionsContainer>
         {hidden ? null : <CartDropdown />}
-    </div>
+    </HeaderContainer>
 )
 
 const mapStateToProps = createStructuredSelector({
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 
 import Directory from '../../components/directory/directory.component'
 
-import './homepage.styles.scss'
+import {HomePageContainer} from './homepage.styles'
 
-const HomePage = () => {
-    return (
-        <div className='homepage'>
-            <h1>Welcome to my Homepage</h1>
-            <Directory />
-        </div>
-    )
-}
+const HomePage = () => (
+    <HomePageContainer>
+        <h1>Welcome to my Homepage</h1>
+        <Directory />
+    </HomePageContainer>
+)
 
 export default HomePage
